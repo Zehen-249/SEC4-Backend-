33d5a610f406b1b8bd0a5703dd3e30475f394035
Mehendi Hasan <117719513+Zehen-249@users.noreply.github.com>
2024-05-14 00:29:06 +0530

    Delete static directory

--- static/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const navTogle = document.querySelector("#navTgl");
-const sidebar = document.querySelector("#leftSideBar");
-
-navTogle.onclick = () => {
-  console.log(sidebar);
-  navTogle.classList.toggle("bi-x-lg");
-};
